28bf45d8c55dfa74444ebe200e40dd010dea1c44
David Abdurachmanov <David.Abdurachmanov@cern.ch>
2016-03-08 23:53:16 +0100

    Remove all references to other libraries expect libCLHEP.so
    In CMS we only care about fat libCLHEP.so (shared) binary, the rest are
    removed from our CLHEP package. We have to make sure that *-config files
    are not have references to removed libraries. Same also applies to
    exported targets in CMake files.
    
    Signed-off-by: David Abdurachmanov <David.Abdurachmanov@cern.ch>
    
    Conflicts:
    	cmake/Modules/ClhepBuildLibrary.cmake

--- cmake/Modules/ClhepBuildLibrary.cmake
+++ cmake/Modules/ClhepBuildLibrary.cmake
@@ -43,15 +43,6 @@ macro(clhep_build_library package)
 
   target_link_libraries(${package}  ${package_library_list} )
   target_link_libraries(${package}S ${package_library_list_static} )
-
-  # Install the libraries
-  INSTALL (TARGETS ${package} ${package}S
-      EXPORT CLHEPLibraryDepends
-      RUNTIME DESTINATION bin
-      LIBRARY DESTINATION lib${LIB_SUFFIX}
-      ARCHIVE DESTINATION lib${LIB_SUFFIX}
-      INCLUDES DESTINATION include
-      ) 
 endmacro(clhep_build_library)
 
 macro(clhep_build_libclhep )
@@ -75,7 +66,7 @@ macro(clhep_build_libclhep )
       )
 
   # export creates library dependency files for CLHEPConfig.cmake
-  INSTALL(TARGETS CLHEP CLHEPS
+  INSTALL(TARGETS CLHEP
       EXPORT CLHEPLibraryDepends
       RUNTIME DESTINATION bin
       LIBRARY DESTINATION lib${LIB_SUFFIX}
